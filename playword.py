ddf3a3ba51845f9d07dc4e76c65947f29674a50f
Adri Martin <adrianmartinparra@gmail.com>
2010-05-29 12:42:25 +0100

    Añadida la funcionalidad básica para empaquetar el texto recogido del clipboard, mandarlo a traducir y mostrar el resultado.

--- playword.py
+++ playword.py
@@ -1,13 +1,44 @@
 # playword.py
 
 import gtk
+import urllib
+import json
 
 
-def _clipboard_changed(clipboard, event):
-    text = clipboard.wait_for_text()
-    print text
+
+LANG_FROM = "en" 
+LANG_TO   = "es"
+
+
+def translate(text_from, lang_from, lang_to):
+    user_ip   = "http://www.my-ajax-site.com"
+    version   = "1.0"
+
+    text_to   = "ERROR"
+    lang_pair = "|".join([lang_from, lang_to])
+    base_url  = "http://ajax.googleapis.com/ajax/services/language/translate?"
+
+    params   = urllib.urlencode({'v':version, 'q': text_from,
+                             'langpair': lang_pair, 'userip':
+                             user_ip})  
+    response = urllib.urlopen(base_url + "%s" % params)
+    response = response.read()
+    data     = json.loads(response)
+
+    if data["responseStatus"] == 200:
+        text_to = data["responseData"]["translatedText"]
+
+    return text_to
+
+      
+def clipboard_changed(clipboard, event):
+    text_from = clipboard.wait_for_text()
+    text_to   = translate(text_from, LANG_FROM, LANG_TO)
+
+    print text_to
+
 
 clip = gtk.clipboard_get(gtk.gdk.SELECTION_PRIMARY)
-clip.connect("owner-change", _clipboard_changed)
+clip.connect("owner-change", clipboard_changed)
 
 gtk.main()
